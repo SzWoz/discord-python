4c2a557eccad82323617adef9968e3d7e790b337
Szymon Woźny <szwoz2001@gmail.com>
2026-06-12 14:18:15 +0200

    fix

--- discord_clone/asgi.py
+++ discord_clone/asgi.py
@@ -2,15 +2,18 @@ import os
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "discord_clone.settings")
 
+from django.core.asgi import get_asgi_application
+
+django_asgi_app = get_asgi_application()
+
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
 
 from chat.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter(
     {
-        "http": get_asgi_application(),
+        "http": django_asgi_app,
         "websocket": AuthMiddlewareStack(URLRouter(websocket_urlpatterns)),
     }
 )
